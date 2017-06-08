ab1946acc21ddc59c2a88ea96eafd1b9bf37ad21
Dylan Lewis and Nicole Freed <nicole@>
2017-06-08 10:10:49 -0700

    add function to convert single digits 1 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 // business logic goes here
+var romans = [[1, "I"], [5, "V"], [10, "X"], [50, "L"], [100, "C"], [500, "D"], [1000, "M"]];
+
 function numCheck(number) {
   if (isNaN(number)) {
     alert("Please enter a number.");
   };
+};
+
+function singleDigit(number) {
+  var result = "";
+  if (number === 1) {
+    result = "I";
+  } else if (number === 5) {
+    result = "V";
+  };
+  return result;
 };
 
 
@@ -13,6 +25,7 @@ $(document).ready(function() {
     var number = parseInt($("input#number-string").val());
     console.log(number);
     numCheck(number);
-
+    singleDigit(number);
+    $("#result").text(singleDigit(number));
   });
 });
