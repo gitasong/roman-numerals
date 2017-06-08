ac543b3bf8fdb9afe88c7ec8d24e0ad770edbf51
Dylan Lewis and Nicole Freed <nicole@>
2017-06-08 09:38:44 -0700

    add JQuery to check for non-numerical values

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+// business logic goes here
+if (isNaN(number) === true) {
+  alert("Please enter a number.");
+}
+
+
+
+// front-end logic goes here
 $(document).ready(function() {
   $("#roman").submit(function(event) {
     event.preventDefault();
