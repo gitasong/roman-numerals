2f0e8ad74b6cd39836cee418d32bafe2d9962bbe
Dylan Lewis and Nicole Freed <dylan@>
2017-06-08 14:11:11 -0700

    create function for each rank of array, modify multiDigit function to handle four digit numbers

--- js/scripts.js
+++ js/scripts.js
@@ -24,27 +24,108 @@ function singleDigit(number) {
   } else if (number === 1000) {
     result = "M";
   };
+  console.log(result);
   return result;
 };
 
-function multiSymbol(number) {
+function onesRank(digit) {
   var result = "";
-  var primitives = [[1, "I"], [2, "II"], [3, "III"], [4, "IV"], [5, "V"], [6, "VI"], [7, "VII"], [8, "VIII"], [9, "IX"], [10, "X"]];
-  // var numberArray = number.toString().split("");
-  //   console.log(numberArray);
-  if (number < 4) {
-    result = "I".repeat(number);
-  } else if (number === 4) {
-    result = "IV";
-  } else if (number === 5) {
-    result = "V";
-  } else if (number < 9) {
-    result = "V" + "I".repeat(number % 5);
-  } else if (number === 9) {
-    result = "IX";
-  }
+  // var primitives = [[1, "I"], [2, "II"], [3, "III"], [4, "IV"], [5, "V"], [6, "VI"], [7, "VII"], [8, "VIII"], [9, "IX"], [10, "X"]];
+
+// for rank -1
+  if (digit < 4) {
+    result = "I".repeat(digit);  // X  // C
+  } else if (digit === 4) {
+    result = "IV";  // XL  // CD
+  } else if (digit === 5) {
+    result = "V";  // L  // D
+  } else if (digit < 9) {
+    result = "V" + "I".repeat(digit % 5);  // LX  //  DC
+  } else if (digit === 9) {
+    result = "IX";  // XC  // CM
+  } else if (digit === 10) {
+    result = "X";
+  };
+  console.log(result);
   return result;
 };
+
+function tensRank(digit) {
+  // for rank -2
+  var result = "";
+  if (digit < 4) {
+    result = "X".repeat(digit);  // X  // C
+  } else if (digit === 4) {
+    result = "XL";  // XL  // CD
+  } else if (digit === 5) {
+    result = "L";  // L  // D
+  } else if (digit < 9) {
+    result = "L" + "X".repeat(digit % 5);  // LX  //  DC
+  } else if (digit === 9) {
+    result = "XC";  // XC  // CM
+  } else if (digit === 10) {
+    result = "C";
+  };
+  console.log(result);
+  return result;
+};
+
+function hundredsRank(digit) {
+// for rank -3
+  var result = "";
+  if (digit < 4) {
+    result = "C".repeat(digit);  // X  // C
+  } else if (digit === 4) {
+    result = "CD";  // XL  // CD
+  } else if (digit === 5) {
+    result = "D";  // L  // D
+  } else if (digit < 9) {
+    result = "D" + "C".repeat(digit % 5);  // LX  //  DC
+  } else if (digit === 9) {
+    result = "CM";  // XC  // CM
+  } else if (digit === 10) {
+    result = "M";
+  };
+  console.log(result);
+  return result;
+};
+
+function thousandsRank(digit) {
+  // for rank -4
+  var result = "";
+  if (digit < 4) {
+    result = "M".repeat(digit);  // X  // C
+  };
+  return result;
+};
+
+function multiDigit(number) {
+  // var concatRoman = [];
+  var numberArray = number.toString().split("");  // converts number to string, then splits string into array of digits
+    console.log(numberArray);
+  // for (digit = 0; digit <= numberArray.length; digit += 1) {  // looping through digits of number in array
+    // concatRoman += multiSymbol(parseInt(numberArray[digit]));  // concatenating Roman equivalents of each digit
+    // console.log(concatRoman);
+    var onesDigit = parseInt(numberArray[3]);
+      console.log(onesDigit);
+    var tensDigit = parseInt(numberArray[2]);
+      console.log(tensDigit);
+    var hundredsDigit = parseInt(numberArray[1]);
+      console.log(hundredsDigit);
+    var thousandsDigit = parseInt(numberArray[0]);
+      console.log(thousandsDigit);
+    var giantConcat = thousandsRank(thousandsDigit) + hundredsRank(hundredsDigit) + tensRank(tensDigit) + onesRank(onesDigit);
+  // };
+  console.log(giantConcat);
+  return giantConcat;
+};
+
+// numberArray.length - (numberArray.index + 1)
+// -(numberArray.index + 1)
+// nunberArray[-1] * 1
+// numberArray[-2] * 10
+// numberArray[-3] * 100
+// numberArray[-4] * 1000
 
 
 
@@ -53,10 +134,10 @@ $(document).ready(function() {
   $("#roman").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number-string").val());
-    console.log(number);
+    // console.log(number);
     // numCheck(number);
     // singleDigit(number);
     // multiSymbol(number);
-    $("#result").text(singleDigit(number));
+    $("#result").text(multiDigit(number));
   });
 });
