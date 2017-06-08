aefa88a3b7a54d217b7eda0d2c8cc22f5f1411d3
Dylan Lewis and Nicole Freed <nicole@>
2017-06-08 11:01:08 -0700

    add Javascript to handle single digit numbers < 10

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,26 @@ function singleDigit(number) {
   return result;
 };
 
+function multiSymbol(number) {
+  var result = "";
+  var primitives = [[1, "I"], [2, "II"], [3, "III"], [4, "IV"], [5, "V"], [6, "VI"], [7, "VII"], [8, "VIII"], [9, "IX"], [10, "X"]];
+  // var numberArray = number.toString().split("");
+  //   console.log(numberArray);
+  if (number < 4) {
+    result = "I".repeat(number);
+  } else if (number === 4) {
+    result = "IV";
+  } else if (number === 5) {
+    result = "V";
+  } else if (number < 9) {
+    result = "V" + "I".repeat(number % 5);
+  } else if (number === 9) {
+    result = "IX";
+  }
+  return result;
+};
+
+
 
 // front-end logic goes here
 $(document).ready(function() {
@@ -24,8 +44,9 @@ $(document).ready(function() {
     event.preventDefault();
     var number = parseInt($("input#number-string").val());
     console.log(number);
-    numCheck(number);
-    singleDigit(number);
-    $("#result").text(singleDigit(number));
+    // numCheck(number);
+    // singleDigit(number);
+    // multiSymbol(number);
+    $("#result").text(multiSymbol(number));
   });
 });
